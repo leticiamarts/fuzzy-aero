cc120d68b8b3b7a78f7fa591e190fa3a8b70c5e6
leticiamarts <leticiamarts99@gmail.com>
2023-09-28 19:56:06 -0300

    refactor: update code (EN)

--- aero.py
+++ aero.py
@@ -3,205 +3,167 @@ from skfuzzy import control as ctrl
 import matplotlib.pyplot as plt
 import skfuzzy as fuzz
 import numpy as np
+import os
 
-# Entrada:
-frequencia = ctrl.Antecedent(np.arange(0, 8, 1), "frequencia")
-tempo_disponivel = ctrl.Antecedent(np.arange(20, 91, 1), "tempo_disponivel")
-excesso_peso = ctrl.Antecedent(np.arange(0, 41, 1), "excesso_peso")
-adesao = ctrl.Antecedent(np.arange(0, 8, 1), "adesao")
-condicionamento = ctrl.Antecedent(np.arange(16, 81, 1), "condicionamento")
+# Inputs:
+frequency = ctrl.Antecedent(np.arange(0, 8, 1), "frequency")
+available_time = ctrl.Antecedent(np.arange(20, 91, 1), "available_time")
+excess_weight = ctrl.Antecedent(np.arange(0, 41, 1), "excess_weight")
+adherence = ctrl.Antecedent(np.arange(0, 8, 1), "adherence")
+fitness = ctrl.Antecedent(np.arange(16, 81, 1), "fitness")
 
-# Saída:
-intensidade = ctrl.Consequent(np.arange(30, 91, 1), "intensidade")
+# Output:
+intensity = ctrl.Consequent(np.arange(30, 91, 1), "intensity")
 
-# Fuzzificação
-# Entrada:
-frequencia["baixa"] = fuzz.trapmf(frequencia.universe, [0, 0, 2, 4])
-frequencia["moderada"] = fuzz.trimf(frequencia.universe, [2, 4, 6])
-frequencia["alta"] = fuzz.trapmf(frequencia.universe, [4, 6, 7, 7])
-tempo_disponivel["pouco"] = fuzz.trimf(tempo_disponivel.universe, [20, 20, 40])
-tempo_disponivel["medio"] = fuzz.trimf(tempo_disponivel.universe, [30, 40, 50])
-tempo_disponivel["muito"] = fuzz.trapmf(tempo_disponivel.universe, [40, 60, 90, 90])
-excesso_peso["pouco"] = fuzz.trimf(excesso_peso.universe, [0, 0, 15])
-excesso_peso["moderado"] = fuzz.trimf(excesso_peso.universe, [5, 15, 30])
-excesso_peso["muito"] = fuzz.trapmf(excesso_peso.universe, [15, 30, 40, 40])
-adesao["baixa"] = fuzz.trimf(adesao.universe, [0, 0, 3.5])
-adesao["moderada"] = fuzz.trimf(adesao.universe, [0, 3.5, 7])
-adesao["alta"] = fuzz.trimf(adesao.universe, [3.5, 7, 7])
-condicionamento["baixo"] = fuzz.trapmf(condicionamento.universe, [16, 16, 20.5, 29.5])
-condicionamento["baixo_medio"] = fuzz.trimf(
-    condicionamento.universe, [20.5, 29.5, 38.5]
-)
-condicionamento["medio"] = fuzz.trimf(condicionamento.universe, [29.5, 38.5, 47.5])
-condicionamento["medio_alto"] = fuzz.trapmf(
-    condicionamento.universe, [38.5, 47.5, 56.5, 56.5]
-)
-condicionamento["alto"] = fuzz.trapmf(condicionamento.universe, [47.5, 56.5, 80, 80])
+# Fuzzification
+# Inputs:
+frequency["low"] = fuzz.trapmf(frequency.universe, [0, 0, 2, 4])
+frequency["moderate"] = fuzz.trimf(frequency.universe, [2, 4, 6])
+frequency["high"] = fuzz.trapmf(frequency.universe, [4, 6, 7, 7])
+available_time["low"] = fuzz.trimf(available_time.universe, [20, 20, 40])
+available_time["medium"] = fuzz.trimf(available_time.universe, [30, 40, 50])
+available_time["high"] = fuzz.trapmf(available_time.universe, [40, 60, 90, 90])
+excess_weight["low"] = fuzz.trimf(excess_weight.universe, [0, 0, 15])
+excess_weight["moderate"] = fuzz.trimf(excess_weight.universe, [5, 15, 30])
+excess_weight["high"] = fuzz.trapmf(excess_weight.universe, [15, 30, 40, 40])
+adherence["low"] = fuzz.trimf(adherence.universe, [0, 0, 3.5])
+adherence["moderate"] = fuzz.trimf(adherence.universe, [0, 3.5, 7])
+adherence["high"] = fuzz.trimf(adherence.universe, [3.5, 7, 7])
+fitness["low"] = fuzz.trapmf(fitness.universe, [16, 16, 20.5, 29.5])
+fitness["low_medium"] = fuzz.trimf(fitness.universe, [20.5, 29.5, 38.5])
+fitness["medium"] = fuzz.trimf(fitness.universe, [29.5, 38.5, 47.5])
+fitness["medium_high"] = fuzz.trapmf(fitness.universe, [38.5, 47.5, 56.5, 56.5])
+fitness["high"] = fuzz.trapmf(fitness.universe, [47.5, 56.5, 80, 80])
 
-# Saída
-intensidade["baixa"] = fuzz.trapmf(intensidade.universe, [30, 30, 37.5, 47.5])
-intensidade["baixa_media"] = fuzz.trimf(intensidade.universe, [37.5, 47.5, 62.5])
-intensidade["media"] = fuzz.trimf(intensidade.universe, [47.5, 62.5, 72.5])
-intensidade["media_alta"] = fuzz.trimf(intensidade.universe, [62.5, 72.5, 85])
-intensidade["alta"] = fuzz.trapmf(intensidade.universe, [72.5, 85, 90, 90])
+# Output
+intensity["low"] = fuzz.trapmf(intensity.universe, [30, 30, 37.5, 47.5])
+intensity["low_medium"] = fuzz.trimf(intensity.universe, [37.5, 47.5, 62.5])
+intensity["medium"] = fuzz.trimf(intensity.universe, [47.5, 62.5, 72.5])
+intensity["medium_high"] = fuzz.trimf(intensity.universe, [62.5, 72.5, 85])
+intensity["high"] = fuzz.trapmf(intensity.universe, [72.5, 85, 90, 90])
 
-# Inferência
-regra1 = ctrl.Rule(condicionamento["alto"], intensidade["alta"])
-regra2 = ctrl.Rule(condicionamento["medio_alto"], intensidade["media_alta"])
-regra3 = ctrl.Rule(condicionamento["medio"], intensidade["media"])
-regra4 = ctrl.Rule(condicionamento["baixo_medio"], intensidade["baixa_media"])
-regra5 = ctrl.Rule(condicionamento["baixo"], intensidade["baixa"])
-regra6 = ctrl.Rule(
-    tempo_disponivel["muito"] & condicionamento["alto"], intensidade["media_alta"]
+# Inference
+rule1 = ctrl.Rule(fitness["high"], intensity["high"])
+rule2 = ctrl.Rule(fitness["medium_high"], intensity["medium_high"])
+rule3 = ctrl.Rule(fitness["medium"], intensity["medium"])
+rule4 = ctrl.Rule(fitness["low_medium"], intensity["low_medium"])
+rule5 = ctrl.Rule(fitness["low"], intensity["low"])
+rule6 = ctrl.Rule(available_time["high"] & fitness["high"], intensity["medium_high"])
+rule7 = ctrl.Rule(available_time["high"] & fitness["medium_high"], intensity["medium"])
+rule8 = ctrl.Rule(available_time["high"] & fitness["medium"], intensity["low_medium"])
+rule9 = ctrl.Rule(frequency["low"] & fitness["high"], intensity["high"])
+rule10 = ctrl.Rule(frequency["low"] & fitness["medium_high"], intensity["high"])
+rule11 = ctrl.Rule(frequency["low"] & fitness["medium"], intensity["medium_high"])
+rule12 = ctrl.Rule(frequency["low"] & fitness["low_medium"], intensity["medium"])
+rule13 = ctrl.Rule(frequency["low"] & fitness["low"], intensity["low_medium"])
+rule14 = ctrl.Rule(available_time["low"] & fitness["high"], intensity["high"])
+rule15 = ctrl.Rule(available_time["low"] & fitness["medium_high"], intensity["high"])
+rule16 = ctrl.Rule(available_time["low"] & fitness["medium"], intensity["medium_high"])
+rule17 = ctrl.Rule(available_time["low"] & fitness["low_medium"], intensity["medium"])
+rule18 = ctrl.Rule(available_time["low"] & fitness["low"], intensity["low_medium"])
+rule19 = ctrl.Rule(frequency["high"] & fitness["high"], intensity["medium_high"])
+rule20 = ctrl.Rule(frequency["high"] & fitness["medium_high"], intensity["medium"])
+rule21 = ctrl.Rule(frequency["high"] & fitness["medium"], intensity["low_medium"])
+rule22 = ctrl.Rule(frequency["high"] & fitness["low_medium"], intensity["low"])
+rule23 = ctrl.Rule(adherence["high"] & fitness["high"], intensity["high"])
+rule24 = ctrl.Rule(adherence["high"] & fitness["medium_high"], intensity["high"])
+rule25 = ctrl.Rule(adherence["high"] & fitness["medium"], intensity["medium_high"])
+rule26 = ctrl.Rule(adherence["high"] & fitness["low_medium"], intensity["medium"])
+rule27 = ctrl.Rule(adherence["high"] & fitness["low"], intensity["low_medium"])
+rule28 = ctrl.Rule(adherence["low"] & fitness["high"], intensity["medium_high"])
+rule29 = ctrl.Rule(adherence["low"] & fitness["medium_high"], intensity["medium"])
+rule30 = ctrl.Rule(adherence["low"] & fitness["medium"], intensity["low_medium"])
+rule31 = ctrl.Rule(adherence["low"] & fitness["low_medium"], intensity["low"])
+rule32 = ctrl.Rule(adherence["moderate"] & fitness["high"], intensity["high"])
+rule33 = ctrl.Rule(
+    adherence["moderate"] & fitness["medium_high"], intensity["medium_high"]
 )
-regra7 = ctrl.Rule(
-    tempo_disponivel["muito"] & condicionamento["medio_alto"], intensidade["media"]
+rule34 = ctrl.Rule(adherence["moderate"] & fitness["medium"], intensity["medium"])
+rule35 = ctrl.Rule(
+    adherence["moderate"] & fitness["low_medium"], intensity["low_medium"]
 )
-regra8 = ctrl.Rule(
-    tempo_disponivel["muito"] & condicionamento["medio"], intensidade["baixa_media"]
+rule36 = ctrl.Rule(adherence["moderate"] & fitness["low"], intensity["low"])
+rule37 = ctrl.Rule(excess_weight["moderate"] & fitness["low"], intensity["low_medium"])
+rule38 = ctrl.Rule(
+    excess_weight["moderate"] & fitness["low_medium"], intensity["medium"]
 )
-regra9 = ctrl.Rule(frequencia["baixa"] & condicionamento["alto"], intensidade["alta"])
-regra10 = ctrl.Rule(
-    frequencia["baixa"] & condicionamento["medio_alto"], intensidade["alta"]
+rule39 = ctrl.Rule(
+    excess_weight["moderate"] & fitness["medium"], intensity["medium_high"]
 )
-regra11 = ctrl.Rule(
-    frequencia["baixa"] & condicionamento["medio"], intensidade["media_alta"]
-)
-regra12 = ctrl.Rule(
-    frequencia["baixa"] & condicionamento["baixo_medio"], intensidade["media"]
-)
-regra13 = ctrl.Rule(
-    frequencia["baixa"] & condicionamento["baixo"], intensidade["baixa_media"]
-)
-regra14 = ctrl.Rule(
-    tempo_disponivel["pouco"] & condicionamento["alto"], intensidade["alta"]
-)
-regra15 = ctrl.Rule(
-    tempo_disponivel["pouco"] & condicionamento["medio_alto"], intensidade["alta"]
-)
-regra16 = ctrl.Rule(
-    tempo_disponivel["pouco"] & condicionamento["medio"], intensidade["media_alta"]
-)
-regra17 = ctrl.Rule(
-    tempo_disponivel["pouco"] & condicionamento["baixo_medio"], intensidade["media"]
-)
-regra18 = ctrl.Rule(
-    tempo_disponivel["pouco"] & condicionamento["baixo"], intensidade["baixa_media"]
-)
-regra19 = ctrl.Rule(
-    frequencia["alta"] & condicionamento["alto"], intensidade["media_alta"]
-)
-regra20 = ctrl.Rule(
-    frequencia["alta"] & condicionamento["medio_alto"], intensidade["media"]
-)
-regra21 = ctrl.Rule(
-    frequencia["alta"] & condicionamento["medio"], intensidade["baixa_media"]
-)
-regra22 = ctrl.Rule(
-    frequencia["alta"] & condicionamento["baixo_medio"], intensidade["baixa"]
-)
-regra23 = ctrl.Rule(adesao["alta"] & condicionamento["alto"], intensidade["alta"])
-regra24 = ctrl.Rule(adesao["alta"] & condicionamento["medio_alto"], intensidade["alta"])
-regra25 = ctrl.Rule(
-    adesao["alta"] & condicionamento["medio"], intensidade["media_alta"]
-)
-regra26 = ctrl.Rule(
-    adesao["alta"] & condicionamento["baixo_medio"], intensidade["media"]
-)
-regra27 = ctrl.Rule(
-    adesao["alta"] & condicionamento["baixo"], intensidade["baixa_media"]
-)
-regra28 = ctrl.Rule(
-    adesao["baixa"] & condicionamento["alto"], intensidade["media_alta"]
-)
-regra29 = ctrl.Rule(
-    adesao["baixa"] & condicionamento["medio_alto"], intensidade["media"]
-)
-regra30 = ctrl.Rule(
-    adesao["baixa"] & condicionamento["medio"], intensidade["baixa_media"]
-)
-regra31 = ctrl.Rule(
-    adesao["baixa"] & condicionamento["baixo_medio"], intensidade["baixa"]
-)
-regra32 = ctrl.Rule(adesao["moderada"] & condicionamento["alto"], intensidade["alta"])
-regra33 = ctrl.Rule(
-    adesao["moderada"] & condicionamento["medio_alto"], intensidade["media_alta"]
-)
-regra34 = ctrl.Rule(adesao["moderada"] & condicionamento["medio"], intensidade["media"])
-regra35 = ctrl.Rule(
-    adesao["moderada"] & condicionamento["baixo_medio"], intensidade["baixa_media"]
-)
-regra36 = ctrl.Rule(adesao["moderada"] & condicionamento["baixo"], intensidade["baixa"])
-regra37 = ctrl.Rule(
-    excesso_peso["moderado"] & condicionamento["baixo"], intensidade["baixa_media"]
-)
-regra38 = ctrl.Rule(
-    excesso_peso["moderado"] & condicionamento["baixo_medio"], intensidade["media"]
-)
-regra39 = ctrl.Rule(
-    excesso_peso["moderado"] & condicionamento["medio"], intensidade["media_alta"]
-)
-regra40 = ctrl.Rule(
-    excesso_peso["moderado"] & condicionamento["medio_alto"], intensidade["alta"]
+rule40 = ctrl.Rule(
+    excess_weight["moderate"] & fitness["medium_high"], intensity["high"]
 )
 
-intensidade_ctrl = ctrl.ControlSystem(
+intensity_ctrl = ctrl.ControlSystem(
     [
-        regra1,
-        regra2,
-        regra3,
-        regra4,
-        regra5,
-        regra6,
-        regra7,
-        regra8,
-        regra9,
-        regra10,
-        regra11,
-        regra12,
-        regra13,
-        regra14,
-        regra15,
-        regra16,
-        regra17,
-        regra18,
-        regra19,
-        regra20,
-        regra21,
-        regra22,
-        regra23,
-        regra24,
-        regra25,
-        regra26,
-        regra27,
-        regra28,
-        regra29,
-        regra30,
-        regra31,
-        regra32,
-        regra33,
-        regra34,
-        regra35,
-        regra36,
-        regra37,
-        regra38,
-        regra39,
-        regra40,
+        rule1,
+        rule2,
+        rule3,
+        rule4,
+        rule5,
+        rule6,
+        rule7,
+        rule8,
+        rule9,
+        rule10,
+        rule11,
+        rule12,
+        rule13,
+        rule14,
+        rule15,
+        rule16,
+        rule17,
+        rule18,
+        rule19,
+        rule20,
+        rule21,
+        rule22,
+        rule23,
+        rule24,
+        rule25,
+        rule26,
+        rule27,
+        rule28,
+        rule29,
+        rule30,
+        rule31,
+        rule32,
+        rule33,
+        rule34,
+        rule35,
+        rule36,
+        rule37,
+        rule38,
+        rule39,
+        rule40,
     ]
 )
-intensidade_simulador = ctrl.ControlSystemSimulation(intensidade_ctrl)
+intensity_simulator = ctrl.ControlSystemSimulation(intensity_ctrl)
 
-### Simulação
+### Simulation
 
-intensidade_simulador.input["frequencia"] = 6  # 0 - 7
-intensidade_simulador.input["tempo_disponivel"] = 28  # 20 - 90
-intensidade_simulador.input["excesso_peso"] = 26  # 0 - 40
-intensidade_simulador.input["adesao"] = 7  # 0 - 8
-intensidade_simulador.input["condicionamento"] = 59  # 16 - 80
+intensity_simulator.input["frequency"] = 6  # 0 - 7
+intensity_simulator.input["available_time"] = 28  # 20 - 90
+intensity_simulator.input["excess_weight"] = 26  # 0 - 40
+intensity_simulator.input["adherence"] = 7  # 0 - 8
+intensity_simulator.input["fitness"] = 59  # 16 - 80
 
-# Deffuzificação
-intensidade_simulador.compute()
-print(intensidade_simulador.output["intensidade"])
-output_value = intensidade_simulador.output["intensidade"]
-intensidade.view(sim=intensidade_simulador)
-plt.savefig("intensidade2.png")
+# Defuzzification
+intensity_simulator.compute()
+print(intensity_simulator.output["intensity"])
+output_value = intensity_simulator.output["intensity"]
+intensity.view(sim=intensity_simulator)
+
+base_filename = "intensity_plot"
+output_folder = "output"
+counter = 1
+
+os.makedirs(output_folder, exist_ok=True)
+
+while True:
+    filename = os.path.join(output_folder, f"{base_filename}{counter}.png")
+    if not os.path.exists(filename):
+        break
+    counter += 1
+
+plt.savefig(filename)
